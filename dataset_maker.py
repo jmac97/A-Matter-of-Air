6e26c592ef0bdab7ad2529d07e72f32bc139e039
jmac97 <mceldoon@gmail.com>
2023-01-27 12:15:19 -0500

    last commit did not include these files

--- dataset_maker.py
+++ dataset_maker.py
@@ -3,28 +3,39 @@ import xlrd
 
 book = xlrd.open_workbook("data/365_PM_2_5_Values_VisualizeSpaceTimeCube3D1.xls")
 sheet = book.sheet_by_index(0)
-amountOfData = sheet.row_values(12)
+amountOfData = sheet.row_values(13)
 
 data = sheet.row_values(1)
 
+inc_amount = 0
+end_range = 0
 
-if amountOfData[0] == 12:
-    print("{")
-    data = sheet.col_values(5)
+print(amountOfData[1])
+if amountOfData[1] == 0:
+   inc_amount = 12
+   end_range = 13
+elif amountOfData[1] == 12:
+   inc_amount = 13
+   end_range = 14
+else:
+   print("range error")
 
-    inc = 0
-    for x in range(1, 43):
-        for y in range(1, 13):
-            if y == 1 or y == 2:
-                continue
-            else: 
-                # print("{")
-                if y != 12:
-                    if y == 3:
-                        print("{", end='')
-                    print(data[y+inc], ",", end='')
-                else:
-                    print(data[y+inc], end='')
-        print("},")
-        inc += 12
-    print("}")
+
+print("{")
+data = sheet.col_values(5)
+
+inc = 0
+for x in range(1, 43):
+   for y in range(1, end_range):
+      if y == 1:
+         print("{", end='')
+         if end_range == 13:
+            print("0.0, ", end='')
+         print(data[y+inc], ",", end='')
+      elif y != inc_amount:
+         print(data[y+inc], ",", end='')
+      elif y == inc_amount:
+         print(data[y+inc], end='')
+   print("},")
+   inc += inc_amount
+print("};")
