5bb360cb85244d6756251fd958059ac729beffdd
jmac97 <mceldoon@gmail.com>
2023-02-14 19:27:17 -0500

    updated string type, tested button

--- string_script/string_script.ino
+++ string_script/string_script.ino
@@ -4,7 +4,7 @@
 // String info
 #define NUM_STRINGS 42
 #define NUM_LEDS_PER_STRING 13
-#define LED_TYPE WS2811
+#define LED_TYPE WS2812
 CRGB leds[NUM_STRINGS][NUM_LEDS_PER_STRING];
 
 // Color info
@@ -332,47 +332,47 @@ void setup() {
   attachPCINT(digitalPinToPCINT(button_pm25), pm25_ISR, CHANGE);
   attachPCINT(digitalPinToPCINT(button_death), death_ISR, CHANGE);
 
-  FastLED.addLeds<LED_TYPE, 0, RGB>(leds[0], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 1, RGB>(leds[1], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 2, RGB>(leds[2], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 3, RGB>(leds[3], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 4, RGB>(leds[4], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 5, RGB>(leds[5], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 6, RGB>(leds[6], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 7, RGB>(leds[7], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 8, RGB>(leds[8], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 9, RGB>(leds[9], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 10, RGB>(leds[10], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 11, RGB>(leds[11], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 12, RGB>(leds[12], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 14, RGB>(leds[14], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 15, RGB>(leds[15], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 16, RGB>(leds[16], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 17, RGB>(leds[17], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 18, RGB>(leds[18], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 19, RGB>(leds[19], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 20, RGB>(leds[20], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 21, RGB>(leds[21], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 22, RGB>(leds[22], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 23, RGB>(leds[23], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 24, RGB>(leds[24], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 25, RGB>(leds[25], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 26, RGB>(leds[26], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 27, RGB>(leds[27], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 28, RGB>(leds[28], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 29, RGB>(leds[29], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 30, RGB>(leds[30], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 31, RGB>(leds[31], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 32, RGB>(leds[32], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 33, RGB>(leds[33], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 34, RGB>(leds[34], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 35, RGB>(leds[35], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 36, RGB>(leds[36], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 37, RGB>(leds[37], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 38, RGB>(leds[38], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 39, RGB>(leds[39], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 40, RGB>(leds[40], NUM_LEDS_PER_STRING);
-  FastLED.addLeds<LED_TYPE, 41, RGB>(leds[41], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 0, GRB>(leds[0], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 1, GRB>(leds[1], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 2, GRB>(leds[2], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 3, GRB>(leds[3], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 4, GRB>(leds[4], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 5, GRB>(leds[5], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 6, GRB>(leds[6], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 7, GRB>(leds[7], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 8, GRB>(leds[8], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 9, GRB>(leds[9], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 10, GRB>(leds[10], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 11, GRB>(leds[11], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 12, GRB>(leds[12], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 14, GRB>(leds[14], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 15, GRB>(leds[15], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 16, GRB>(leds[16], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 17, GRB>(leds[17], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 18, GRB>(leds[18], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 19, GRB>(leds[19], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 20, GRB>(leds[20], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 21, GRB>(leds[21], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 22, GRB>(leds[22], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 23, GRB>(leds[23], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 24, GRB>(leds[24], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 25, GRB>(leds[25], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 26, GRB>(leds[26], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 27, GRB>(leds[27], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 28, GRB>(leds[28], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 29, GRB>(leds[29], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 30, GRB>(leds[30], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 31, GRB>(leds[31], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 32, GRB>(leds[32], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 33, GRB>(leds[33], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 34, GRB>(leds[34], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 35, GRB>(leds[35], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 36, GRB>(leds[36], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 37, GRB>(leds[37], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 38, GRB>(leds[38], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 39, GRB>(leds[39], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 40, GRB>(leds[40], NUM_LEDS_PER_STRING);
+  FastLED.addLeds<LED_TYPE, 41, GRB>(leds[41], NUM_LEDS_PER_STRING);
 
   FastLED.setBrightness(100);
 
@@ -380,9 +380,10 @@ void setup() {
 
   clear();
 
-  //  colorTest();
-  //    dataTest(0);
-  //    show_dataset(death);
+//  colorTest();
+
+//      dataTest(0);
+      show_dataset(death);
 
   FastLED.show();
 }
@@ -403,9 +404,11 @@ void clear() {
 }
 
 void colorTest() {
-  for (int i = 0; i < 7; i++) {
-    leds[0][i] = colors[i];
+
+  for (int i = 0; i < 13; i++) {
+    leds[3][i] = colors[6];
   }
+
 }
 
 void dataTest(int string) {
@@ -419,7 +422,7 @@ void dataTest(int string) {
 void show_dataset(const char dataset[][13]) {
   for (int i = 0; i < NUM_STRINGS; i++) {
     for (int j = 0; j < NUM_LEDS_PER_STRING; j++) {
-      leds[i][NUM_LEDS_PER_STRING - j] = colors[pgm_read_byte(&(dataset[i][j]))];
+      leds[i][NUM_LEDS_PER_STRING-1-j] = colors[pgm_read_byte(&(dataset[i][j]))];
     }
   }
 }
@@ -447,10 +450,12 @@ void cycle() {
 
 void pm25_ISR() {
   delay(150);
-  Serial.println("pm25");
+  show_dataset(pm25);
+  FastLED.show();
 }
 
 void death_ISR() {
   delay(150);
-  Serial.println("death");
+  show_dataset(death);
+  FastLED.show();
 }
